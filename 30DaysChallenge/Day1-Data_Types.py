35ae2298f5d7bc81e5d9d0f46bc1f9f7146cc65e
Warlen Marcio Pinheiro <warlen@live.com>
2020-03-27 13:18:54 -0300

    Create Day1-Data_Types.py

---
+++ 30DaysChallenge/Day1-Data_Types.py
@@ -0,0 +1,14 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Read and save an integer, double, and String to your variables.
+secondInt = int(input())
+secondFloat = float(input())
+secondString = input()
+# Print the sum of both integer variables on a new line.
+print(i+secondInt)
+# Print the sum of the double variables on a new line.
+print(d+secondFloat)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+secondString)

9488a41bf372e56937f3e6122b16b027733dc600
Warlen Marcio Pinheiro <warlen@live.com>
2020-04-23 14:57:12 -0300

    Update Day8-DictionariesAndMaps.py

--- 30DaysChallenge/Day8-DictionariesAndMaps.py
+++ 30DaysChallenge/Day8-DictionariesAndMaps.py
@@ -1,14 +1,15 @@
+import sys
+
 n = int(input())
-mydict = {}
-for i in range(0, n):
-    data = input().split(" ")
-    key = data[0]
-    value = data[1]
+mydict = dict()
+for _ in range(0, n):
+    key, value = input().split(" ")
     mydict[key] = value
 
-for i in range(0, n):
-    key = input()
+key = sys.stdin.readline().strip()
+while key:
     if key in mydict:
         print(key+"="+mydict[key])
     else:
         print("Not found")
+    key = sys.stdin.readline().strip()
